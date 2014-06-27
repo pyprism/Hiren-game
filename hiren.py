b444e22dfa5c368080653f96c1c8377de57d1ad0
Ashutosh Das <pyprism@users.noreply.github.com>
2014-06-27 15:11:26 +0600

    Fixed CORS issue

--- hiren.py
+++ hiren.py
@@ -13,7 +13,49 @@ collection = db['game']
 
 app = Flask(__name__)
 
-@app.route("/",  methods=['POST', 'GET'])
+#Fix for 'Access-Control-Allow-Origin'
+def crossdomain(origin=None, methods=None, headers=None, max_age=21600, attach_to_all=True, automatic_options=True):  
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
+
+
+@app.route("/",  methods=['POST', 'GET', 'OPTIONS'])
+@crossdomain(origin='*')
 def create():
     if request.method == 'POST':
         name = request.form['name']
@@ -30,7 +72,8 @@ def create():
         return 'Fuck off!' + " " + str(datetime.datetime.utcnow())
 
 
-@app.route('/create_game', methods=['POST'])
+@app.route('/create_game', methods=['POST', 'OPTIONS'])
+@crossdomain(origin='*')
 def create_game():
     if request.method == 'POST':
         id = request.form['id']
@@ -44,7 +87,8 @@ def create_game():
             return jsonify({'status': 'already exits '}), 302
 
 
-@app.route("/update_score",  methods=['POST', 'GET'])
+@app.route("/update_score",  methods=['POST', 'GET', 'OPTIONS'])
+@crossdomain(origin='*')
 def update_score():
     if request.method == 'POST':
         id = request.form['id']
@@ -55,7 +99,8 @@ def update_score():
         return jsonify({'status': 'voila babe , score updated !'}), 202
 
 
-@app.route("/result", methods=['POST'])
+@app.route("/result", methods=['POST', 'OPTIONS'])
+@crossdomain(origin='*')
 def result():
     if request.method == 'POST':
         id = request.form['id']
